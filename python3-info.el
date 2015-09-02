ba97ba139f812be6640239723f7c0828d3fee1e7
David Hansen <david.hansen@gmx.net>
2015-09-02 20:11:38 +0200

    New files.

---
+++ python3-info.el
@@ -0,0 +1,13 @@
+;; python3-info.el --- Python 3 info manual for Emacs
+
+
+;;; Commentary:
+;; This file is deliberately empty, since this package just contains a
+;; Python manual.  Emacs, however, requires us to have *something* that
+;; describes the package.
+
+;;; Code:
+
+(provide 'python3-info)
+
+;;; python-info.el ends here
